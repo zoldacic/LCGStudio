c895a3ef587459b1fe3505a8eb3956eb47bcec59
Fredrik Lundgren <k.o.f.lundgren@gmail.com>
2017-05-07 21:49:02 +0200

    Copy from old repo

---
+++ Assets/scripts/NetworkManager_Custom.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+using UnityEngine.UI;
+using System;
+
+public class NetworkManager_Custom : NetworkManager {
+    public void StartupHost() {
+        SetPort();
+        NetworkManager.singleton.StartHost();
+    }   
+
+    public void JoinGame()
+    {
+        SetIPAddress();
+        SetPort();
+        NetworkManager.singleton.StartClient();
+    }
+
+    private void SetIPAddress()
+    {
+        var ipAddress = GameObject.Find("InputFieldIPAddress").transform.FindChild("Text").GetComponent<Text>().text;
+        NetworkManager.singleton.networkAddress = ipAddress;
+    }
+
+    private void SetPort()
+    {
+        NetworkManager.singleton.networkPort = 7777;
+    }
+
+    private void OnLevelWasLoaded(int level)
+    {
+        if (level == 0)
+        {
+            //SetupMenuSceneButtons();
+        } else
+        {
+            //SetupOtherSceneButtons();
+        }
+    }
+
+    private void SetupOtherSceneButtons()
+    {
+        GameObject.Find("ButtonStartHost").GetComponent<Button>().onClick.RemoveAllListeners();
+        GameObject.Find("ButtonStartHost").GetComponent<Button>().onClick.AddListener(StartupHost);
+
+        GameObject.Find("ButtonJoinGame").GetComponent<Button>().onClick.RemoveAllListeners();
+        GameObject.Find("ButtonJoinGame").GetComponent<Button>().onClick.AddListener(JoinGame);
+    }
+
+    private void SetupMenuSceneButtons()
+    {
+        GameObject.Find("ButtonDisconnect").GetComponent<Button>().onClick.RemoveAllListeners();
+        GameObject.Find("ButtonDisconnect").GetComponent<Button>().onClick.AddListener(NetworkManager.singleton.StopHost);
+    } 
+}
