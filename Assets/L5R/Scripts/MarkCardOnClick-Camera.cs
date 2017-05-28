cbf1740dcfc1c68e983f2c805b2f11a66a390346
zoldacic <k.o.f.lundgren@gmail.com>
2017-05-29 00:29:42 +0200

    .

---
+++ Assets/L5R/Scripts/MarkCardOnClick-Camera.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace Assets.L5R.Scripts
+{
+    class MarkCardOnClick_Camera : MonoBehaviour
+    {
+        //void Update()
+        //{
+        //    if (Input.GetMouseButtonDown(0))
+        //    {
+        //        //Vector2 mouseWorldPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        //        RaycastHit2D hit = Physics2D.Raycast(
+        //            new Vector2(
+        //                Camera.main.ScreenToWorldPoint(Input.mousePosition).x,
+        //                Camera.main.ScreenToWorldPoint(Input.mousePosition).y),
+        //                Vector2.zero,
+        //                0);
+
+        //        if (hit)
+        //        {
+        //            Debug.Log("Name = " + hit.collider.gameObject.name);
+
+        //            //Debug.Log("Name = " + hit.collider.name);
+        //            //Debug.Log("Tag = " + hit.collider.tag);
+        //            //Debug.Log("Hit Point = " + hit.point);
+        //            //Debug.Log("Object position = " + hit.collider.gameObject.transform.position);
+        //            //Debug.Log("--------------");
+        //        }
+        //    }
+        //}
+
+        void Update()
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+                Vector2 mousePos2D = new Vector2(mousePos.x, mousePos.y);
+
+                RaycastHit2D hit = Physics2D.Raycast(mousePos2D, Vector2.zero);
+                if (hit.collider != null)
+                {
+                    Debug.Log(hit.collider.gameObject.name);
+                    hit.collider.attachedRigidbody.AddForce(Vector2.up);
+                }
+            }
+        }
+    }
+}
