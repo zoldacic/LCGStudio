e7410fe7392adb685542e4b1e038524d40d5b896
zoldacic <k.o.f.lundgren@gmail.com>
2017-06-18 00:17:43 +0200

    .

--- Assets/L5R/Scripts/CardLoader.cs
+++ Assets/L5R/Scripts/CardLoader.cs
@@ -9,6 +9,8 @@ namespace Assets.SimpleDragAndDrop.Scripts
 {
     class CardLoader : MonoBehaviour
     {
+        private string baseUrl = @"http://fiveringsdb.com/api/";
+
         private List<string> CrabDynasty = new List<string> {
             "Borderlands Defender",
             "Borderlands Fortifications",
@@ -26,8 +28,37 @@ namespace Assets.SimpleDragAndDrop.Scripts
                 card.GetComponent<Image>().sprite = Sprite.Create(img, new Rect(0.0f, 0.0f, img.width, img.height), new Vector2(0.0f, 0.0f));
                 card.transform.parent = parent.transform;
             });
+        }
 
+        public void LoadImage(string clan, string type, string cardName)
+        {
+            GameObject card = (GameObject)Instantiate(Resources.Load("Prefabs/Card")); ;
+            Texture2D img = (Texture2D)Resources.Load("Images/" + clan + "/" + type + "/" + cardName);
 
+            card.GetComponent<Image>().sprite = Sprite.Create(img, new Rect(0.0f, 0.0f, img.width, img.height), new Vector2(0.0f, 0.0f));
+            //card.GetComponent<CardInfo>().sprite = 
+
+            // Find Parent By Name
+
+            card.transform.parent = parent.transform;
+        }
+
+        public void LoadCardInfo()
+        {             
+            var www = new WWW(baseUrl + "cards");
+            //yield return www;           
+
+            while (!www.isDone)
+                System.Threading.Thread.Sleep(100);
+
+            // Use Edit > Paste Special to create class
+
+            var cards = JsonUtility.FromJson<>(www.text).Cards;
+
+            foreach (card in cards)
+            {
+                LoadImage(card.clan_code, card.side, card.code);
+            }
         }
     }
 }
